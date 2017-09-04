442b5d60c257e9456793592473efff34d2aca196
DarkyZ <s.soulie@hotmail.fr>
2017-09-05 01:15:57 +0200

    WIP Cleaning : add moderator route

---
+++ routes/moderator/dashboard.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function get(req, res)
+{
+    res.render('moderator/dashboard');
+}
+
+module.exports = get;
--- routes/moderator/index.js
+++ routes/moderator/index.js
@@ -3,5 +3,6 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', require('./dashboard.js'));
 
 module.exports = router;
