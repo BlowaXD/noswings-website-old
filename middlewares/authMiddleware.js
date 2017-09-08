5880a1d74287f248f69c252c2bc3d6dfb838d45e
DarkyZ <s.soulie@hotmail.fr>
2017-09-08 16:09:10 +0200

    End of auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ const Cookies = require('cookies');
 function authRequired(req, res, next)
 {
     const cookies = new Cookies(req, res);
-    const token = cookies.get('token');
+    const token = cookies.get(`${global.config.server}-token`);
 
     /* No token */
     if (!token)
@@ -15,25 +15,28 @@ function authRequired(req, res, next)
     if (params.length !== 3)
         return res.redirect(req.protocol + '://' + req.get('host') + '/login');
 
+    /* Parse token */
+    let user;
     try
     {
-        const data = new Buffer(params[1], 'base64').toString('utf-8');
-
-        console.log(data);
+        user = JSON.parse(new Buffer(params[1], 'base64').toString('utf-8'));
     }
     catch (e)
     {
         return res.redirect(req.protocol + '://' + req.get('host') + '/login');
     }
 
+    /* Check fields */
+    if (!user || !user.username || !user.hashedPassword || !user.exp)
+        return res.redirect(req.protocol + '://' + req.get('host') + '/login');
 
-    /* Test token
-    ** - check expiration date
-    ** - check 'basic' variables (username, hashedPassword, permissions)'
-    **
-    ** OK : set req.user
-    ** KO : return res.redirect(req.protocol + '://' + req.get('host') + '/login')
-    */
+    /* Check expiration date */
+    if (Date.now() > parseInt(user.exp) * 1000)
+        return res.redirect(req.protocol + '://' + req.get('host') + '/login');
+
+    /* That's ok */
+    req.user = user;
+    req.user.token = token;
     next();
 }
 
