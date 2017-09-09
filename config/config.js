a409f63a2ae1bc83819503ede12397c5d059bec3
DarkyZ <s.soulie@hotmail.fr>
2017-09-10 00:53:49 +0200

    Get user info from API

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ const api_host = 'http://localhost:4500';
 module.exports = {
     server: 'NosWings',
     api: {
+        get_info: `${api_host}/user/get_info`,
         get_token: `${api_host}/user/token`,
         get_packs: `${api_host}/shop/packs`,
         post_buy: `${api_host}/shop/buy`,
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 'use strict';
 const Cookies = require('cookies');
+const request = require('request');
 
 function authRequired(req, res, next)
 {
@@ -34,18 +35,42 @@ function authRequired(req, res, next)
     if (Date.now() > parseInt(user.exp) * 1000)
         return res.redirect(req.protocol + '://' + req.get('host') + '/login');
 
-    /* That's ok */
-    const permissions = user.permissions;
+    /* Check token */
+    const opt = {
+        method: 'get',
+        url: global.config.api.get_info,
+        headers: { 'x-access-token': token }
+    };
 
-    if (permissions)
-    {
+    request(opt, (err, response, body) => {
+        if (err || response.statusCode !== 200)
+        {
+            console.log(response ? response.statusCode : '', err);
+            return res.redirect(req.protocol + '://' + req.get('host') + '/login');
+        }
+
+        let info;
+        try
+        {
+            info = JSON.parse(body);
+            if (!info.success)
+                throw new Error('Not succeed');
+            info = info.data;
+        }
+        catch (e)
+        {
+            console.log(e);
+            return res.redirect(req.protocol + '://' + req.get('host') + '/login');
+        }
+
+        user.money = info.money || null;
         user.permissions = {};
-        user.permissions.IS_ADMIN = (permissions & global.config.e_permissions.IS_ADMIN) === 1;
-        user.permissions.IS_GM = (permissions & global.config.e_permissions.IS_GM >> 1) === 1;
-    }
-    req.user = user;
-    req.user.token = token;
-    next();
+        user.permissions.IS_ADMIN = (info.permissions & global.config.e_permissions.IS_ADMIN) === 1;
+        user.permissions.IS_GM = (info.permissions & global.config.e_permissions.IS_GM >> 1) === 1;
+        req.user = user;
+        req.user.token = token;
+        next();
+    });
 }
 
 module.exports = authRequired;
