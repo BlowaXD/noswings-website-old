f70566c3b98606e86e3a4240fe5061ce7c95d45c
DarkyZ <s.soulie@hotmail.fr>
2017-09-10 17:24:05 +0200

    Fix error page

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function (err, req, res, next) {
     console.error(err);
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('error', { title: 'Error', user: {} });
 });
 
 module.exports = app;
