308a734c74f504589d21d0b0c4f5e81c25f48064
Blowa <blowaxd693@gmail.com>
2017-09-15 21:26:57 +0200

    [FIX] Misuse

--- routes/website/index.js
+++ routes/website/index.js
@@ -11,14 +11,14 @@ router.use('/register', require('./register.js'));
 router.use('/forgotten', require('./forgotten.js'));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+router.use(function (req, res, next) {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+router.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
