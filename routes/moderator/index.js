677aac78bf9fb66dbde434c54b66481e1134fc69
DarkyZ <s.soulie@hotmail.fr>
2017-09-15 23:39:24 +0200

    Fixed Modo middleware ('cuz Blowa c pas coder)

--- routes/moderator/index.js
+++ routes/moderator/index.js
@@ -6,12 +6,9 @@ const router = express.Router();
 
 router.use(auth_middleware);
 router.use((req, res, next) => {
-    if (req.user.permissions >= global.config.e_permissions.IS_GM)
-    {
-        return (next);
-    }
-    else
-        res.redirect(req.protocol + '://' + req.get('host') + '/');
+    if (req.user.permissions.IS_GM)
+        return next;
+    res.redirect(req.protocol + '://' + req.get('host') + '/');
 });
 router.get('/', require('./dashboard.js'));
 
