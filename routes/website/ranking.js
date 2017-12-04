5310c4b04fc0527d904afe2877f11a142291a50a
Blowa <blowaxd693@gmail.com>
2017-12-04 17:49:55 +0100

    fix ranking xss & localisation

--- routes/website/ranking.js
+++ routes/website/ranking.js
@@ -2,8 +2,7 @@
 
 const requestp = require('request-promise');
 
-function get(req, res)
-{
+function get(req, res) {
     const data = {
         title: global.translate.WEBSITE.RANKING.TITLE,
         description: global.translate.WEBSITE.RANKING.DESC
@@ -11,22 +10,27 @@ function get(req, res)
     res.render('website/ranking', data);
 }
 
-async function getRanks(req, res)
-{
+async function getRanks(req, res) {
     const opt = {
         method: 'get',
         url: global.config.api.get_ranking + "/" + req.params.rankingType
     };
-
     await requestp(opt)
-        .then(function (body)
-        {
+        .then(function (body) {
             res.status(200).json({
                 success: true,
-                data: JSON.parse(body).data
+                data: JSON.parse(body).data.map((character) => {
+                    return {
+                        Name: character.Name,
+                        Level: character.Level,
+                        HeroLevel: character.HeroLevel,
+                        LevelXp: character.LevelXp,
+                        Reputation: character.Reputation,
+                        Class: res.__(character.Class)
+                    };
+                })
             });
-        }).catch(function (err)
-        {
+        }).catch(function (err) {
             res.status(500).json(err.error);
         });
 }
